e0a19ddead19c068a3fa3fdd93cf503c946fc57b
mhier <mhier@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-10-14 13:55:31 +0000

    added documentation
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/VirtualLab/trunk@1714 5f601a5c-88b9-47f3-b851-818f6741d00d

--- include/SignalSink.h
+++ include/SignalSink.h
@@ -13,15 +13,17 @@
 
 namespace mtca4u { namespace VirtualLab {
 
+  /// A SignalSink requensts time-dependent signal values from a SignalSource. Objects of this class will normally be
+  /// members of a VirtualLabBackend or a model component.
   class SignalSink {
 
     public:
 
-      /// Constructor: create sink already connected with a source.
-      SignalSink(boost::shared_ptr<SignalSource> &source);
-
       /// Constructor: create sink with default value returned when not connected
       SignalSink(double defaultValue);
+
+      /// Constructor: create sink already connected with a source.
+      SignalSink(boost::shared_ptr<SignalSource> &source);
 
       /// [call from VirtualLab setup code] (re-)connect this SignalSink with a SignalSource. Any previous connection
       /// to another source will be severed.
