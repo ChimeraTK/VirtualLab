fdfe6e8feb560842f27d32e217d00057a4e53077
mhier <mhier@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-12-03 13:07:23 +0000

    improved Doxygen documentation for macros
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/VirtualLab/trunk@1893 5f601a5c-88b9-47f3-b851-818f6741d00d

--- include/VirtualLabBackend.h
+++ include/VirtualLabBackend.h
@@ -33,21 +33,21 @@ using namespace boost::msm::front::euml;        // this is required when using b
 namespace msm = boost::msm;
 namespace mpl = boost::mpl;
 
-/**
+/** \def DECLARE_EVENT(name)
  * Declare an event. Use instead of BOOST_MSM_EUML_EVENT, as we must not create instances for the events as well.
  *
  * (test coverage hint: this macro is used in the test) */
 #define DECLARE_EVENT(name)                                                                                     \
   class name : public msm::front::euml::euml_event< name > {};
 
-/**
+/** \def DECLARE_STATE(name)
  * Declare a plain state, without any entry or exit functions etc.
  *
  * (test coverage hint: this macro is used in the test) */
 #define DECLARE_STATE(name)                                                                                     \
   class name : public msm::front::state<> , public msm::front::euml::euml_state<name> {};
 
-/**
+/** \def DECLARE_LOGGING_STATE(name)
  * Declare a logging state. Entry and exit of this state will be looged to std::cout.
  *
  * (test coverage hint: this macro is used in the test, but output is not actually tested) */
@@ -60,7 +60,7 @@ namespace mpl = boost::mpl;
       void on_exit(Event const&,FSM&) { std::cout << "Leaving state: " << #name << std::endl; }                 \
   };
 
-/**
+/** \def DECLARE_REGISTER(UserType, name)
  * Declare a dummy register accessor for single-word or 1D-array registers.
  * UserType is the data type the data should be accessed by. The conversion is handled internally using the
  * FixedPointConverter.
@@ -68,7 +68,7 @@ namespace mpl = boost::mpl;
  * (test coverage hint: this macro is used in the test) */
 #define DECLARE_REGISTER(UserType, name) mtca4u::DummyRegisterAccessor<UserType> name;
 
-/**
+/** \def DECLARE_MUXED_REGISTER(UserType, name)
  * Declare a dummy register accessor for multiplexed 2D-array registers.
  * UserType is the data type the data should be accessed by. The conversion is handled internally using the
  * FixedPointConverter.
@@ -76,7 +76,7 @@ namespace mpl = boost::mpl;
  * (test coverage hint: this macro is used in the test) */
 #define DECLARE_MUXED_REGISTER(UserType, name) mtca4u::DummyMultiplexedRegisterAccessor<UserType> name;
 
-/**
+/** \def WRITEEVENT_TABLE
  * Provide a "table" of events and register names using the CONNECT_REGISTER_EVENT macro for write events.
  * The table must be terminated with END_WRITEEVENT_TABLE.
  *
@@ -87,7 +87,7 @@ namespace mpl = boost::mpl;
 
 #define END_WRITEEVENT_TABLE }
 
-/**
+/** \def READEVENT_TABLE
  * Provide a "table" of events and register names using the CONNECT_REGISTER_EVENT macro for read events
  * The table must be terminated with END_READEVENT_TABLE.
  *
@@ -98,7 +98,7 @@ namespace mpl = boost::mpl;
 
 #define END_READEVENT_TABLE }
 
-/**
+/** \def CONNECT_REGISTER_EVENT(eventName, regsterAccessor)
  * Connect events with registers. The first argument eventName is the name of an event previously declared using
  * BOOST_MSM_EUML_EVENT. The second argument is a DummyRegisterAccessor for the register to connect with. Use this
  * macro in the of the WRITE_EVENT_TABLE and READ_EVENT_TABLE macros.
@@ -113,7 +113,7 @@ namespace mpl = boost::mpl;
     }                                                                                                           \
   }
 
-/**
+/** \def DECLARE_REGISTER_GUARD(guardName, condition)
  * Declare a guard condition on the value of a register. The resulting guard condition can be used on an event
  * defined with the CONNECT_REGISTER_EVENT macro. The first argument guardName is the name of the resulting guard
  * class and the second argument will be parsed as the guard condition (standard C++ syntax). Inside the condition,
@@ -136,7 +136,7 @@ namespace mpl = boost::mpl;
       return ( condition );                                                                                     \
     }
 
-/**
+/** \def DECLARE_GUARD(guardName)
  * Declare a guard condition. The argument guardName is the name of the resulting guard class.
  * The code must follow this macro, contain a return statement returning a boolean and has to be terminated
  * with END_DECLARE_GUARD.
@@ -155,7 +155,7 @@ namespace mpl = boost::mpl;
 
 #define END_DECLARE_GUARD }
 
-/**
+/** \def DECLARE_ACTION(actionName)
  * Declare an action with arbitrary code. The argument actionName is the name of the resulting action class.
  * The code must follow this macro and has to be terminated with END_DECLARE_ACTION.
  *
@@ -173,13 +173,13 @@ namespace mpl = boost::mpl;
 
 #define END_DECLARE_ACTION }
 
-/**
+/** \def DECLARE_TIMER(name,event)
  * Declare a timer with a given name. Will fire the given event.
  *
  * (test coverage hint: this macro is used in the test) */
 #define DECLARE_TIMER(name,event) Timer<event> name;
 
-/**
+/** \def DECLARE_TIMER_GROUP(name, ...)
  * Declare a timer group with a given name. The additional arguments must be the timers part of
  * this group, previously declared using DECLARE_TIMER.
  *
@@ -200,7 +200,7 @@ namespace mpl = boost::mpl;
     };                                                                                                          \
     name ## _ name;
 
-/**
+/** \def DECLARE_STATE_MACHINE(stateMachineName, initialState, transitionTable)
  * Declare a state machine (usually a sub-state machine, also see DECLARE_MAIN_STATE_MACHINE)
  * dummyDeviceType is the class name of the physDummyDevice implementation the state machine will be used in
  * stateMachineName is the name of the state machine itself. Several types will be defined based on this names with
@@ -226,7 +226,7 @@ namespace mpl = boost::mpl;
     };                                                                                                          \
     typedef msm::back::state_machine<stateMachineName ## _> stateMachineName;
 
-/**
+/** \def DECLARE_MAIN_STATE_MACHINE(initialState, transitionTable)
  * Declare the main state machine. This is just like DECLARE_STATE_MACHINE but with a fixed name "mainStateMachine".
  * Also the state machine will be instantiated under the name "theStateMachine", as expected by other parts of this
  * framework.
@@ -250,7 +250,7 @@ namespace mpl = boost::mpl;
     typedef msm::back::state_machine<mainStateMachine_> mainStateMachine;                                       \
     mainStateMachine theStateMachine;
 
-/**
+/** \def CONSTRUCTOR(name,...)
  * Declare the constructor of the VirtualLabBackend. The first argument must be the class name. The other arguments
  * must be the list of member initialisers, the code of the cunstructor follows this macro and must be terminated
  * with END_CONSTRUCTOR, even if no code is put into the constructor.
@@ -310,14 +310,14 @@ namespace mpl = boost::mpl;
 
 #define END_CONSTRUCTOR }
 
-/**
+/** \def INIT_SUB_STATE_MACHINE(name)
  * Initialise a sub-state machine. This should be called inside of the constructor.
  *
  * (test coverage hint: this macro is used in the test) */
 #define INIT_SUB_STATE_MACHINE(name)                                                                            \
     theStateMachine.get_state< name * >()->setDummyDevice(this);
 
-/**
+/** \def REGISTER_BACKEND_TYPE(name)
  * Register backend type with the BackendFactory. Must be placed into the C++ source file for any VirtualLabBackend
  * even when not intending to use the backend factory (not into the header file)!
  *
@@ -329,14 +329,32 @@ namespace mpl = boost::mpl;
 namespace mtca4u { namespace VirtualLab {
 
 /*********************************************************************************************************************/
-/** The dummy device opens a mapping file instead of a device, and
- *  implements all registers defined in the mapping file in memory.
- *  Like this it mimics the real PCIe device.
+/** Base class for VirtualLab dummy devices.
  *
- *  This class helps implementing dummy devices using a state machine and allows an easy connection with the
- *  virtual lab simulation framework.
+ *  The VirtualLabBackend is an extension of the DummyBackend. Like its base class, it implements a set of registers
+ *  defined in a map file in memory. VirtualLabBackend helps implementing the functionality, which will be provided
+ *  by the firmware and hardware in case of a real device. The implementation of firmware functionality is realised
+ *  using a state machine. Inputs and outputs of the simulated device can be added using the SignalSink and SignalSource
+ *  classes, which can then be connected with other VirtualLab components in the virtual lab setup code.
  *
- *  The physDummyDevice class is a template of the derived implementation, which means it follows a CRTP
+ *  Use the following macros to build your VirtualLabBackend:
+ *  - \ref CONSTRUCTOR to create the constructor including parts needed for registration with the BackendFactory
+ *  - \ref REGISTER_BACKEND_TYPE needs to be placed in the C++ file to register with the BackendFactory
+ *  - \ref DECLARE_REGISTER and DECLARE_MUXED_REGISTER to obtain register accessors for registers
+ *  - \ref DECLARE_MAIN_STATE_MACHINE to declare your main state machine with transition table
+ *  - \ref DECLARE_STATE_MACHINE to declare any sub state machine
+ *  - \ref DECLARE_STATE, \ref DECLARE_EVENT, \ref DECLARE_GUARD and \ref DECLARE_ACTION to build state machine
+ *         components
+ *  - \ref DECLARE_LOGGING_STATE to create states with debug output to the console
+ *  - \ref DECLARE_REGISTER_GUARD to declare a guard condition using a register
+ *  - \ref WRITEEVENT_TABLE, \ref READEVENT_TABLE and \ref CONNECT_REGISTER_EVENT to create events firing on reading
+ *         or writing registers
+ *  - \ref DECLARE_TIMER and \ref DECLARE_TIMER_GROUP to create timers and put them into a group
+ *  - \ref INIT_SUB_STATE_MACHINE inside the \ref CONSTRUCTOR to initialise a sub state machine created with
+ *         \ref DECLARE_STATE_MACHINE
+ *
+ *  Note: The bare minimum usually is one \ref DECLARE_STATE_MACHINE with one \ref DECLARE_STATE, plus one
+ *  \ref DECLARE_TIMER_GROUP containing one timer.
  */
 template<class derived>
 class VirtualLabBackend : public DummyBackend
